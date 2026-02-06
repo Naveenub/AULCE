ec107ded89ceaba8323dd0ddfeed91ca6db454b9
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-02-06 19:29:58 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+return (
+<div>
+<h1>PiedPiperX Compression UI</h1>
+<p>Upload files to compress and view results.</p>
+</div>
+);
+}
+
+export default App;
